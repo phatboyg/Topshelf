aad355ed02bacf25a3da0af9257358cee6652338
Igor <igorm@bluemetal.com>
2012-04-09 18:01:35 -0400

    Added support for the configuration control of the listenning port for the dashboard

--- src/TopShelf.NancyDashboard/TopshelfDashboard.cs
+++ src/TopShelf.NancyDashboard/TopshelfDashboard.cs
@@ -1,6 +1,8 @@
 namespace Topshelf.NancyDashboard
 {
     using System;
+    using System.Configuration;
+    using System.Net;
     using Nancy.Hosting.Self;
     using Topshelf;
     using Topshelf.Logging;
@@ -27,15 +29,49 @@ namespace Topshelf.NancyDashboard
         public void Start()
         {
             ServerUri = new Uri("http://localhost:8085");
+            
+            string listenUrl = ConfigurationManager.AppSettings["DashboardUri"];
+            if (! string.IsNullOrEmpty(listenUrl))
+            {
+                try
+                {
+                    ServerUri = new Uri(listenUrl);
+                }
+                catch (Exception ex)
+                {
+                    _log.Error("Incorrect DashboardUri format. Using the default setting.", ex);
+                }
+            }
+            
             _log.InfoFormat("Loading dashboard at Uri: {0}", ServerUri);
 
-            _nancyHost = new NancyHost(ServerUri);
-            _nancyHost.Start();
+            try
+            {
+                _nancyHost = new NancyHost(ServerUri);
+                _nancyHost.Start();
+            }
+            catch (HttpListenerException ex)
+            {
+                _nancyHost = null;
+                if (ex.Message == "Access is denied")
+                {
+                    string enableCommand = string.Format(@"netsh http add urlacl url=http://+:{0}{1} user=your_domain\service_user", ServerUri.Port, ServerUri.AbsolutePath);
+                    _log.Error("Failed to initialize HTTP listener.\r\nThe process is not allowed listening on the specified port without permission.\r\nConsider running the following command: " + enableCommand, ex);
+                }
+                else
+                    _log.Error("Failed to initialize HTTP listener.", ex);
+            }
+            catch (Exception ex)
+            {
+                _nancyHost = null;
+                _log.Error("Failed to initialize HTTP listener.", ex);
+            }
         }
 
         public void Stop()
         {
-            _nancyHost.Stop();
+            if (_nancyHost != null)
+                _nancyHost.Stop();
             _nancyHost = null;
         }
     }
